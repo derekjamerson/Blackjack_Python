2a6bfa93e959aeb51cfcad0e6fc30a16536b58f6
Jason Ward <jason.louard.ward@gmail.com>
2022-03-08 15:08:22 -0500

    Added simple test for repr version of `Card`

---
+++ tests/test_card.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from card import Card
+
+
+class CardTestCase(TestCase):
+    def test_str_method_returns_text_for_card(self):
+        card = Card(v=1, s='Spades')
+        self.assertEqual(repr(card), '1 of Spades')
