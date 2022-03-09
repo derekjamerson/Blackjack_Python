6f2f980b08251e3378073434b389df8bbeae50ec
Derek Jamerson <derekjamerson@gmail.com>
2022-03-09 15:10:29 -0500

    Card.score and Card.value_name decorated as properties

--- card.py
+++ card.py
@@ -9,18 +9,18 @@ class Card:
     def __init__(self, v, s):
         self.suit = s
         self.value = v
-        self.value_name = self.face_value_mapping.get(self.value, self.value)
-        self.score = self.get_score()
 
-    def __repr__(self):
-        return f'{self.value_name}{self.suit}'
-
-    def get_score(self):
+    @property
+    def score(self):
         if self.value > 10:
             return 10
         elif self.value == 1:
             return 11
-        else:
-            return self.value
+        return self.value
 
+    @property
+    def value_name(self):
+        return self.face_value_mapping.get(self.value, self.value)
 
+    def __repr__(self):
+        return f'{self.value_name}{self.suit}'
