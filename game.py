8e6dc60408c69989dfbc8c81203ff3e63e576904
Derek Jamerson <derekjamerson@gmail.com>
2022-03-10 08:55:14 -0500

    Revert "initial effort. downgrade to python3.8"
    This reverts commit 616902c65185e0e68deb5e96cf9bfcd9e80483dd.
    
    undoing python downgrade

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from operator import attrgetter
+
 from deck import Deck
 from player import Player
 
@@ -25,14 +27,22 @@ class Game:
     def game_over(self):
         p_score = self.player.calculate_score()
         d_score = self.dealer.calculate_score()
-        print("Player: %s %s" % (p_score, self.player.hand))
-        print("Dealer: %s %s" % (d_score, self.dealer.hand))
+        you_win = False
         if d_score < p_score <= 21 or p_score <= 21 < d_score:
-            print("*** YOU WIN ***")
+            you_win = True
             self.score += 1
         else:
-            print("*** YOU LOSE ***")
+            you_win = False
             self.score -= 1
+        final_string = ""
+        if you_win:
+            final_string = "*** YOU WIN ***"
+        else:
+            final_string = "*** YOU LOSE ***"
+        print(final_string)
+        print("Player: %s %s" % (p_score, self.player.hand))
+        print("Dealer: %s %s" % (d_score, self.dealer.hand))
+        print(final_string)
         print("Score: %s" % self.score)
 
     def discard_hands(self):
