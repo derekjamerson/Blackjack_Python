800c10c8036d3fbfc1284f54c315569b17e3bf40
Derek Jamerson <derekjamerson@gmail.com>
2022-03-11 09:20:53 -0500

    added a couple tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -5,13 +5,43 @@ from player import Player
 
 
 class PlayerTestCase(TestCase):
-    def test_all_possible_two_number_card_hand_combinations(self):
-        player = Player()
-        for first in range(2, 10):
-            for second in range(first, 10):
-                player.hand = [Card(first, 1), Card(second, 2)]
-                self.assertEqual(
-                    player.score,
-                    first + second,
-                    f'Error: [{first}, {second}] != {player.score}',
-                )
+    player = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.player = Player()
+
+    def test_player_hand_2_2(self):
+        first_value = 2
+        second_value = 2
+        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
+        self.assertEqual(self.player.score, 4)
+
+    def test_player_hand_10_10(self):
+        first_value = 10
+        second_value = 10
+        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
+        self.assertEqual(self.player.score, 20)
+
+    def test_player_hand_10_a(self):
+        first_value = 10
+        second_value = 1
+        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
+        self.assertEqual(self.player.score, 21)
+
+    def test_player_hand_k_k(self):
+        first_value = 13
+        second_value = 13
+        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
+        self.assertEqual(self.player.score, 20)
+
+    def test_player_hand_a_a(self):
+        first_value = 1
+        second_value = 1
+        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
+        self.assertEqual(self.player.score, 12)
+
+    # def test_player_hand_21_aces(self):
+    #     for num in range(1, 22):
+    #         self.player.hand.append(Card(1, 3))
+    #     self.assertEqual(self.player.score, 21)
