034aa569a4a3c95fb997a9e880b732d517d8bffa
Derek Jamerson <derekjamerson@gmail.com>
2022-03-12 13:37:07 -0500

    show-missing

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 rm -rf .coverage
 coverage run --source=. --branch --omit=*test*.py -m nose2 -v &&
-coverage report --report-missing --fail-under=100 &&
+coverage report --show-missing --fail-under=100 &&
 echo "Success"
