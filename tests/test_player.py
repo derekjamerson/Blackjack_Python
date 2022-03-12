d6c15c0f8a6d247738f15ac46ea70d03072bbcb9
Derek Jamerson <derekjamerson@gmail.com>
2022-03-11 21:04:00 -0500

    added helper to player tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -11,55 +11,23 @@ class PlayerTestCase(TestCase):
     def setUpClass(cls):
         cls.player = Player()
 
-    def test_player_hand_2_2(self):
-        first_value = 2
-        second_value = 2
-        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
-        self.assertEqual(self.player.score, 4)
-
-    def test_player_hand_10_10(self):
-        first_value = 10
-        second_value = 10
-        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
-        self.assertEqual(self.player.score, 20)
-
-    def test_player_hand_10_a(self):
-        first_value = 10
-        second_value = 1
-        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
-        self.assertEqual(self.player.score, 21)
-
-    def test_player_hand_k_k(self):
-        first_value = 13
-        second_value = 13
-        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
-        self.assertEqual(self.player.score, 20)
-
-    def test_player_hand_a_a(self):
-        first_value = 1
-        second_value = 1
-        self.player.hand = [Card(first_value, 1), Card(second_value, 2)]
-        self.assertEqual(self.player.score, 12)
-
-    def test_player_hand_j_q_k(self):
-        first_value = 12
-        second_value = 12
-        third_value = 13
-        self.player.hand = [
-            Card(first_value, 1),
-            Card(second_value, 2),
-            Card(third_value, 3),
-        ]
-        self.assertEqual(self.player.score, 30)
-
-    def test_player_hand_21_aces(self):
+    def assert_hand_score(self, cards, score):
         self.player.hand = []
-        for num in range(21):
-            self.player.hand.append(Card(1, 3))
-        self.assertEqual(self.player.score, 21)
+        for card in cards:
+            self.player.hand.append(Card(card, 1))
+        self.assertEqual(self.player.score, score)
 
-    def test_player_hand_one_of_each(self):
-        self.player.hand = []
-        for num in range(13):
-            self.player.hand.append(Card(num, 1))
-        self.assertEqual(self.player.score, sum(range(13)) - 3)
+    def test_player_hand_number_cards(self):
+        self.assert_hand_score([2, 2], 4)
+        self.assert_hand_score([2, 3, 4, 5, 6, 7, 8, 9], 44)
+        self.assert_hand_score([10, 10], 20)
+        self.assert_hand_score([10, 9], 19)
+
+    def test_player_hand_faces(self):
+        self.assert_hand_score([11, 11], 20)
+        self.assert_hand_score([11, 12, 13], 30)
+
+    def test_player_hand_aces(self):
+        self.assert_hand_score([1, 1], 12)
+        self.assert_hand_score([1, 10], 21)
+        self.assert_hand_score([1, 1, 1, 1, 1], 15)
